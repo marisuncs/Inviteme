14cee439f458e714cda8d708f1f92859e2579bb2
marisuncs <134332015+marisuncs@users.noreply.github.com>
2026-05-19 13:49:47 -0600

    Add files via upload

---
+++ invitacion1/js/app.js
@@ -0,0 +1,17 @@
+const elements = document.querySelectorAll('.fade-up');
+
+const observer = new IntersectionObserver(entries => {
+
+  entries.forEach(entry => {
+
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+
+  });
+
+},{
+  threshold:0.15
+});
+
+elements.forEach(el => observer.observe(el));
