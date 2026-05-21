148403aff29d0cfbcac5b2e548075c6a53c32669
marisuncs <134332015+marisuncs@users.noreply.github.com>
2026-05-21 16:39:17 -0600

    Update IntersectionObserver for fade-up elements
    Refactor IntersectionObserver to add and remove 'visible' class.

--- invitacion1/js/app.js
+++ invitacion1/js/app.js
@@ -1,6 +1,16 @@
 const elements = document.querySelectorAll('.fade-up');
 
-const observer = new IntersectionObserver(entries => {
+const fadeObs = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
+}, { threshold: 0.15 });
+
+/*const observer = new IntersectionObserver(entries => {
 
   entries.forEach(entry => {
 
@@ -14,4 +24,4 @@ const observer = new IntersectionObserver(entries => {
   threshold:0.15
 });
 
-elements.forEach(el => observer.observe(el));
+elements.forEach(el => observer.observe(el));*/
